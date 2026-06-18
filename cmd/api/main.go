1567a1e06f264f1389514c8f6b001bf54140d488
Thehunter101 <thehunter10121@gmail.com>
2026-06-18 02:15:07 -0500

    Se agrego configuracion para usar gorm como orm usando sqlite

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"GoAssistAutoBackend/internal/handlers"
+	//"GoAssistAutoBackend/internal/config"
+	// "GoAssistAutoBackend/internal/models" importacion de los modelos
 )
 
 
 func main(){
+	//Se incia la base de datos
+	//db := config.InitDB()
+	// aqui se migran los modelos de Internal models
+	// db.AutoMigrate(&models.User) user es un ejemplo
 	//Aqui se generan el enrutamiento
 	router := gin.Default()
 	router.GET("/hola/:name",handlers.Hello)
--- internal/config/database.go
+++ internal/config/database.go
@@ -2,4 +2,15 @@ package config
 
 import (
 	"fmt"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
+
+func InitDB() *gorm.DB{
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil{
+		panic("Todo se fue a la vrg!")
+	}
+	fmt.Println("Thas Great!")
+	return db
+}
